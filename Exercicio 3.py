f80f7816e21292918a8bd9b77ff8b0b26ff4f5b6
CesarVerissimo <cesarjunior@unigranrio.br>
2020-08-31 11:10:04 -0300

    Adicionar Exercicio 3

---
+++ Exercicio 3.py
@@ -0,0 +1,5 @@
+descricao = input("Descrição do produto:")
+valor = float(input("Digite o valor unitario:"))
+quantidade = int(input("Digite a quantidade"))
+total = valor * quantidade
+print("Valor a pagar:", total)
